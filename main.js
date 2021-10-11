5af7e020d23444f60e031aba517e86b4f985bde8
stephanie lucano <stephaniemarielucano@gmail.com>
2021-10-11 10:04:08 -0400

    Added the built in fetch() function that allows a user to make a request to wttr.in API and retrieve the current weather and three day forecast for a single search

---
+++ main.js
@@ -0,0 +1,93 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const userInput = event.target.location.value
+    const url = `https://wttr.in/${userInput}?format=j1`
+    fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+        document.querySelector(".display").append(quickSummary(result));
+        document.querySelector(".display").append(threeDayForecast(result));
+        return result
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+function createHeading(description, value) {
+    const heading = document.createElement("h3");
+    heading.textContent = `${description}: ${value}`
+    return heading
+}
+
+function quickSummary(result) {
+    const rmHeading = document.querySelector(".display h2")
+    rmHeading.remove()
+
+    const section = document.createElement("section")
+    section.classList.add("quick-summary")
+
+    const heading = document.createElement("h2");
+    heading.textContent = result.nearest_area[0].areaName[0].value;
+    section.append(heading)
+    
+    const area = createHeading("Area", result.nearest_area[0].areaName[0].value)
+    section.append(area)
+
+    const region = createHeading("Region", result.nearest_area[0].region[0].value)
+    section.append(region)
+
+    const country = createHeading("Country", result.nearest_area[0].country[0].value)
+    section.append(country)
+
+    const currently = createHeading("Currently", `Feels Like ${result.current_condition[0].FeelsLikeF}°F`)
+    section.append(currently)
+    
+    return section;
+}
+
+function threeDayForecast(result) {
+    const section = document.createElement("section")
+    section.classList.add("three-day-forecast")
+
+    const today = document.createElement("h2");
+    today.textContent = "Today"
+    section.append(today)
+
+    const averageTemperature1 = createHeading("Average Temperature", `${result.weather[0].avgtempF}°F`)
+    section.append(averageTemperature1)
+
+    const maxTemperature1 = createHeading("Max Temperature", `${result.weather[0].maxtempF}°F`)
+    section.append(maxTemperature1)
+
+    const minTemperature1 = createHeading("Min Temperature", `${result.weather[0].mintempF}°F`)
+    section.append(minTemperature1)
+
+    const tomorrow = document.createElement("h2");
+    tomorrow.textContent = "Tomorrow"
+    section.append(tomorrow)
+
+    const averageTemperature2 = createHeading("Average Temperature", `${result.weather[1].avgtempF}°F`)
+    section.append(averageTemperature2)
+
+    const maxTemperature2 = createHeading("Max Temperature", `${result.weather[1].maxtempF}°F`)
+    section.append(maxTemperature2)
+
+    const minTemperature2 = createHeading("Min Temperature", `${result.weather[1].mintempF}°F`)
+    section.append(minTemperature2)
+
+    const dayAfterTomorrow = document.createElement("h2");
+    dayAfterTomorrow.textContent = "Day After Tomorrow"
+    section.append(dayAfterTomorrow)
+
+    const averageTemperature3 = createHeading("Average Temperature", `${result.weather[2].avgtempF}°F`)
+    section.append(averageTemperature3)
+
+    const maxTemperature3 = createHeading("Max Temperature", `${result.weather[2].maxtempF}°F`)
+    section.append(maxTemperature3)
+
+    const minTemperature3 = createHeading("Min Temperature", `${result.weather[2].mintempF}°F`)
+    section.append(minTemperature3)
+    
+    return section;
+}
